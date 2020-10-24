439a0a194d75245dd62e5f27669bdc8e379e78c1
hojin Mun <jin1281@gmail.com>
2020-10-23 21:59:11 -0400

    updated port

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -45,7 +45,7 @@ const Projects = () => {
                       <div>
                         <p>
                           {info ||
-                            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi neque, ipsa animi maiores repellendu distinctioaperiam earum dolor voluptatum consequatur blanditiis inventore debitis fuga numquam voluptate architecto itaque molestiae.'}
+                            ''}
                         </p>
                         <p className="mb-4">{info2 || ''}</p>
                       </div>
@@ -53,7 +53,7 @@ const Projects = () => {
                         target="_blank"
                         rel="noopener noreferrer"
                         className="cta-btn cta-btn--hero"
-                        href={url || '#!'}
+                        href={url  || 'https://good-game2020.herokuapp.com/'}
                       >
                         See Live
                       </a>
